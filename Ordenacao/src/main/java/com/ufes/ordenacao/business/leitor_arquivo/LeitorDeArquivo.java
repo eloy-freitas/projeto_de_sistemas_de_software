45d88fce134360cb9932bc8ac2d47092e6f64722
eloy-freitas <freitaseloy.1@gmail.com>
2022-10-12 17:09:04 -0300

    feat: Ordenação Strategy

---
+++ Ordenacao/src/main/java/com/ufes/ordenacao/business/leitor_arquivo/LeitorDeArquivo.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ufes.ordenacao.model;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ *
+ * @author eloy
+ */
+public class LeitorDeArquivo {
+    private File arquivo;
+    private List<Double> numerosSemOrdem;
+    
+    public LeitorDeArquivo(String path) {
+        this.arquivo = new File(path);
+        this.numerosSemOrdem = new ArrayList<>();
+    }
+    
+    public List<Double> getNumerosSemOrdem() throws FileNotFoundException{
+        Scanner scanner = new Scanner(this.arquivo);
+        while(scanner.hasNextInt()){
+            this.numerosSemOrdem.add(scanner.nextDouble());
+        }
+        return this.numerosSemOrdem;
+    }
+    
+}
